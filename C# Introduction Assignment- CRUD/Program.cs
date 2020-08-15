a795173a888bccd1c107909deb519306916f7b25
dumebiitama <didi.itama@gmail.com>
2020-08-15 10:56:43 -0600

    Feature: Sort List, Prevent addition of duplicate item

--- C# Introduction Assignment- CRUD/Program.cs
+++ C# Introduction Assignment- CRUD/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Name: Dumebi Itama (Didi)
+Title: CRUD Assignment
+Last Modified Date: August 15, 2020
+Purpose: C# introduction - decisions, iteration and data structures
+*/
+
+using System;
 using System.Collections;
 
 namespace C__Introduction_Assignment__CRUD
@@ -17,8 +24,7 @@ namespace C__Introduction_Assignment__CRUD
                 Console.WriteLine("type \"2\" and press the enter key to display content of the list");
                 Console.WriteLine("type \"3\" and press the enter key to edit an item in the list");
                 Console.WriteLine("type \"4\" and press the enter key to delete an item in the list");
-                Console.WriteLine("type \"5\" and press the enter key to display content of the list");
-                Console.WriteLine("type \"6\" and press the enter key to exit the program\n\n");
+                Console.WriteLine("type \"5\" and press the enter key to exit the program\n\n");
 
                 string selection = Console.ReadLine().Trim();
                 if(selection.Equals("1")){
@@ -30,8 +36,6 @@ namespace C__Introduction_Assignment__CRUD
                 } else if(selection.Equals("4")){
                     deleteItemInList(listOfData, maxSize);
                 } else if(selection.Equals("5")){
-
-                } else if(selection.Equals("6")){
                     exit = true;
                 } else {
                     Console.WriteLine("Invalid program option. Make correct selection.");
@@ -46,7 +50,15 @@ namespace C__Introduction_Assignment__CRUD
             if (sizeOfList < maxSize){
                 Console.WriteLine("Enter data " + (sizeOfList+1));
                 string data = Console.ReadLine().Trim();
+
+                int duplicateDataIndex = isDataContainedInList(list, data);
+                if(duplicateDataIndex != -1){
+                    Console.WriteLine("\"" + data + "\" cannot be added to list beause \"" + ((string)list[duplicateDataIndex]) + "\" is already contained in the List." );
+                    return;
+                }
+
                 list.Add(data);
+                list.Sort();
                 Console.WriteLine(data + " has been added to list");  
             } else {
                 Console.WriteLine("The size of the list has reached the maximum limit of " + maxSize);
@@ -79,8 +91,16 @@ namespace C__Introduction_Assignment__CRUD
                 string newData = Console.ReadLine().Trim();
                 int dataIndex = itemNumber - 1; 
                 string previousData = (string)list[dataIndex];
+
+                int duplicateDataIndex = isDataContainedInList(list, newData);
+                if(duplicateDataIndex != -1){
+                    Console.WriteLine("\"" + previousData + "\" cannot be edited to " + "\"" + newData + "\" beause \"" + ((string)list[duplicateDataIndex]) + "\" is already contained in the List." );
+                    return;
+                }
+                
                 list.RemoveAt(dataIndex);
                 list.Insert(dataIndex, newData);
+                list.Sort();
                 Console.WriteLine("Item " + (itemNumber) + " has been edited from " + (previousData) + " to " + (newData));
             }
             catch (FormatException e)
@@ -115,6 +135,16 @@ namespace C__Introduction_Assignment__CRUD
                 Console.WriteLine("\n\n" + e.Message);
                 Console.WriteLine("Error: " + input + " cannot be converted to an integer");
             }
+        }
+
+        static int isDataContainedInList(ArrayList list, string data){
+            for(int i = 0; i < list.Count; i++){
+                string item = (string)list[i];
+                if(item.ToLower().Equals(data.ToLower())){
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
